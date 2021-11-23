092e84d7b6216dcde70b4bd0e1081878c92eaef1
CB Smith <guitarded1@outlook.com>
2021-11-23 16:38:27 -0600

    final user_data edit

--- main.tf
+++ main.tf
@@ -20,18 +20,25 @@ locals {
 resource "aws_instance" "fwep_server" {
   ami                    = "ami-0142f6ace1c558c7d"
   instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.main.id
+  #subnet_id              = aws_subnet.main.id
   key_name               = "ec2_oregon"
-  vpc_security_group_ids = ["sg-0e99b28f6a4c1c99f"]
-  
-  #network_interface {
-  #  device_index         = 1
-  #  network_interface_id = aws_network_interface.test.id
-  #}
+  #vpc_security_group_ids = ["sg-0e99b28f6a4c1c99f"]
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.eni0.id
+  }
+  network_interface {
+    device_index         = 1
+    network_interface_id = aws_network_interface.eni1.id
+  }
+
 
   user_data = <<EOF
     #!/bin/bash
-    sudo hostnamectl set-hostname hostname `ifconfig eth0|grep "inet addr"| awk -F ":" '{print $2}'|awk '{print $1}'`
+    IP=`ifconfig eth1|grep "inet "|awk -F " " '{print $2}'`
+    newhostname=$${IP//[.]/-}
+    sudo sysctl kernel.hostname=$newhostname
     
   EOF
   tags = {

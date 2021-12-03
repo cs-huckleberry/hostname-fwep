d2a370703199632df47c9a25dce5f2f30bb7ee8d
CB Smith <guitarded1@outlook.com>
2021-12-03 17:17:27 -0600

    replaced then command with ":"

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "fwep_server" {
       eni1=`ifconfig eth1|grep "inet "|awk -F " " '{print $2}'| sed s/"\."/-/g`
       hostnamectl set-hostname `echo $eni1`
     else
-      echo "Secondary ENI not present"
+      :
     fi
   EOF
   tags = {
